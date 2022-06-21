104f8eb5a488ca3aa511bbfeea9fa0f7ec905883
George Chang <59218296+gchang12@users.noreply.github.com>
2022-06-21 10:04:18 -1000

    Add files via upload

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
-mkdir src 2>/dev/null && (cp src.tar src; cd src; tar xf src.tar; cd ..)
+tar xf src.tar || py pdf-scraper.py
 cd src
 my_rename() {
     for i in $1*.pdf; do mv $i ${i/$1/} 2>/dev/null; done;
@@ -12,5 +12,5 @@ ls src/ | grep [A-J]\.pdf >> files.txt
 echo Index.pdf >> files.txt
 for i in $(cat files.txt); do pdfinfo ./src/$i | grep Pages: | sed s/Pages:\\s*//; done > page-counts.txt
 for i in $(cat files.txt); do less ./src/$i | head -n 1 | sed s/^\\s*//; done > titles.txt
-#vi titles.txt # 24: +" P2P.Wrox.Com"
+vi titles.txt # 24: Append " P2P.Wrox.Com"
 pdflatex main && pdflatex main
