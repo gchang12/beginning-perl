ed1aa7576a89720b594a3411d204f1cd1becd9f5
George Chang <59218296+gchang12@users.noreply.github.com>
2022-07-07 09:32:37 -1000

    Add files via upload

--- create.sh
+++ create.sh
@@ -1,8 +1,9 @@
-tar xf src.tar || py pdf-scraper.py
+tar xf src.tar || py python/pdf-scraper.py
 cd src
 my_rename() {
     for i in $1*.pdf; do mv $i ${i/$1/} 2>/dev/null; done;
 }
+# Strip away all the 3145_, Chap, App, and leading 0's
 my_rename 3145_
 my_rename App && my_rename Chap && my_rename 0
 cd ..
@@ -15,4 +16,5 @@ for i in $(cat files.txt); do less ./src/$i | head -n 1 | sed s/^\\s*//; done > 
 echo Please append "P2P.Wrox.Com" to line 34 of titles.txt. Press the Return key.
 read
 vi titles.txt # 24: Append " P2P.Wrox.Com"
+mv {titles,files,page-counts}.txt stats/
 pdflatex main && pdflatex main
---
+++ python/pdf-scraper.py
@@ -0,0 +1,31 @@
+# https://www.geeksforgeeks.org/downloading-pdfs-with-python-using-requests-and-beautifulsoup/
+
+import re
+
+from bs4 import BeautifulSoup
+from requests import get
+
+from os.path import sep
+from os import mkdir
+
+url='https://www.perl.org/books/beginning-perl/'
+soup=BeautifulSoup(get(url).text,'html.parser')
+
+feature_list=soup.find('div',class_='feature_list')
+
+save_dir='src'
+
+try:
+    mkdir(save_dir)
+except FileExistsError:
+    pass
+
+for a in feature_list.find_all('a'):
+    if 'href' not in a.attrs or 'google' in a['href'] or re.search('.pdf$',a['href']) is None:
+        continue
+    pdf_name=a['href'].split('/')[-1]
+    pdf_name=sep.join([save_dir,pdf_name])
+    pdf=open(pdf_name,'wb')
+    response=get(a.get('href'))
+    pdf.write(response.content)
+    pdf.close()
